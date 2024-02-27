c3289523dbb14f15cc4805de9ab913c91381ac93
alejandropm <geo.al3x@gmail.com>
2024-02-27 16:10:00 +0100

    Add prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="bg-stone-600 w-full h-full">
+      <h1>Hello, world</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
