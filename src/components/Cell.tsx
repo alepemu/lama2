f6aea8aa963ae19649e1d51d3846ed9c765dc509
alejandropm <geo.al3x@gmail.com>
2024-03-04 16:34:25 +0100

    Refactor Cell component to improve readability and remove unnecessary code

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -5,33 +5,25 @@ import { CSS } from "@dnd-kit/utilities";
 import { ItemType } from "../types/types";
 
 export function Cell({ item }: { item: ItemType }) {
-  const sortable = useSortable({
-    id: item.id,
-    animateLayoutChanges: (args) => {
-      // return false;
-      // return args.isSorting
-      return !args.wasDragging;
-    },
-  });
+  const sortable = useSortable({ id: item.id });
 
   return (
-      <div
-        ref={sortable.setNodeRef}
-        style={{
-          backgroundColor: sortable.isDragging ? "red" : "green",
-          transform: sortable.isDragging
-            ? CSS.Translate.toString(sortable.transform)
-            : undefined,
-          opacity:
-            sortable.isOver && sortable.over?.id !== sortable.active?.id
-              ? 0.5
-              : 1,
-        }}
-        {...sortable.attributes}
-        {...sortable.listeners}
-        className="bg-blue-700 p-4 rounded-xl text-sm text-white"
-      >
-        {item.text}
-      </div>
+    <div
+      ref={sortable.setNodeRef}
+      style={{
+        transform: sortable.isDragging
+          ? CSS.Translate.toString(sortable.transform)
+          : undefined,
+        opacity:
+          sortable.isOver && sortable.over?.id !== sortable.active?.id
+            ? 0.5
+            : 1,
+      }}
+      {...sortable.attributes}
+      {...sortable.listeners}
+      className="bg-blue-700 p-4 rounded-xl text-sm text-white"
+    >
+      {item.text}
+    </div>
   );
 }
