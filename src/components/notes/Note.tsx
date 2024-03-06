6cdabbd2dc69514effb3675936e41517b945bd66
alejandropm <geo.al3x@gmail.com>
2024-03-06 16:07:46 +0100

    Add delete functionality to Note component

--- src/components/notes/Note.tsx
+++ src/components/notes/Note.tsx
@@ -1,8 +1,13 @@
 import { forwardRef, CSSProperties } from "react";
 import { NoteProps } from "../../types";
 
+import { useAppDispatch } from "../../hooks/store";
+import { deleteNoteById } from "../../store/notes.slice";
+
 const Note = forwardRef<HTMLDivElement, NoteProps>(
   ({ id, data, isDragging, style, ...props }, ref) => {
+    const dispatch = useAppDispatch();
+
     const inlineStyles: CSSProperties = {
       opacity: isDragging ? "0.75" : "1",
       border: isDragging ? "2px dashed white" : "",
@@ -13,19 +18,27 @@ const Note = forwardRef<HTMLDivElement, NoteProps>(
 
     return (
       <div
-        className="min-w-80 overflow-hidden bg-stone-700 p-4 rounded-xl text-white border-2 border-white/25"
+        className="relative min-w-80 overflow-hidden bg-stone-700 p-4 rounded-xl text-white border-2 border-white/25"
         ref={ref}
         style={inlineStyles}
         {...props}
       >
-        <h1 className="break-words font-bold text-xl">{data.title}</h1>
-        {/* <p>{data.text}</p> */}
-        {/* <ul>
+        <button
+          className="absolute top-1 right-2 text-red-300 opacity-50 cursor-pointer"
+          onClick={() => dispatch(deleteNoteById(id))}
+        >
+          X
+        </button>
+        <div>
+          <h1 className="break-words font-bold text-xl">{data.title}</h1>
+          {/* <p>{data.text}</p> */}
+          {/* <ul>
           {data.list?.map((item, index) => (
             <li key={index}>{item}</li>
           ))}
         </ul> */}
-        <p className="italic opacity-50">Id: {id}</p>
+          <p className="italic opacity-50">Id: {id}</p>
+        </div>
       </div>
     );
   }
