f19f3332301b800f8c7aeab9385966585d986390
alejandropm <geo.al3x@gmail.com>
2024-03-08 16:05:07 +0100

    Update package.json version and Navbar content

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,8 @@
 export function Navbar() {
   return (
-    <div className="h-12 p-2 bg-zinc-800 text-white">
-      <h1 className="text-xl">LAMA2</h1>
+    <div className="flex justify-between items-center h-12 px-4 bg-zinc-800 text-white">
+      <h1 className="text-xl font-bold">LAMA2</h1>
+      <h1 className="text-md opacity-50">v0.1.0</h1>
     </div>
   );
 }
