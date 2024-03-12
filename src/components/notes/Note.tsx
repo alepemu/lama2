2763ed83b5d39d31400e9bf49a0f4e6a8542e600
alejandropm <geo.al3x@gmail.com>
2024-03-12 17:00:56 +0100

    Update favicon link and adjust Note component layout

--- src/components/notes/Note.tsx
+++ src/components/notes/Note.tsx
@@ -29,17 +29,19 @@ const Note = forwardRef<HTMLDivElement, NoteProps>(
         >
           x
         </button>
-        <div>
-          <h1 className="break-words font-bold text-xl">{data.title}</h1>
-          {data.typeId === 0 ? (
-            <p>{data.text}</p>
-          ) : (
-            <ul className="list-disc ml-4">
-              {data.list?.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-          )}
+        <div className="flex flex-col justify-between h-full">
+          <div>
+            <h1 className="break-words font-bold text-xl">{data.title}</h1>
+            {data.typeId === 0 ? (
+              <p>{data.text}</p>
+            ) : (
+              <ul className="list-disc ml-4">
+                {data.list?.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            )}
+          </div>
           <p className="text-right text-xs italic opacity-50">id-{id}</p>
         </div>
       </div>
