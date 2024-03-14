62cc85082b386af7c413b0e66ea01335aeaefc8c
alejandropm <geo.al3x@gmail.com>
2024-03-14 18:17:20 +0100

    Refactor createNewNote

--- src/services/createNewNote.ts
+++ src/services/createNewNote.ts
@@ -1,6 +1,12 @@
 // Types
 import { NoteInputType } from "@/types";
 
+const handleServerError = (newNote: NoteInputType, message: string) => {
+  newNote.data.title = "Oops!";
+  newNote.data.text = message;
+  newNote.data.typeId = 0;
+};
+
 const createNewNote = async (
   input: string,
   typeId: number,
@@ -22,25 +28,18 @@ const createNewNote = async (
       });
 
       if (!response.ok) {
-        newNote.data.title = "Oops!";
-        newNote.data.text = "The server response has failed ):";
-        newNote.data.typeId = 0;
+        handleServerError(newNote, "The server response has failed ):");
+        return newNote;
       }
 
       const data = await response.json();
-      if (data.title) newNote.data.title = data.title;
-      newNote.data.text = data.text;
-      newNote.data.list = data.list;
-      newNote.data.typeId = data.typeId;
+      newNote.data = { ...newNote.data, ...data };
     } catch (error) {
-      newNote.data.title = "Oops!";
-      newNote.data.text = "The server is currently down ):";
-      newNote.data.typeId = 0;
+      handleServerError(newNote, "The server is currently down ):");
     }
   } else if (method === "manual") {
     newNote.data.text = typeId === 0 ? "" : undefined;
     newNote.data.list = typeId === 1 ? [] : undefined;
-    newNote.data.typeId = typeId;
   }
 
   return newNote;
