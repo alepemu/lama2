d4f9d6323b49e9029002ba64c7227acb80e9b546
alejandropm <geo.al3x@gmail.com>
2024-03-14 19:16:36 +0100

    Update Dashboard and Navbar heights

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,7 +2,7 @@ import { version } from "../../../package.json";
 
 export function Navbar() {
   return (
-    <div className="flex justify-between items-center h-12 px-4 bg-zinc-800">
+    <div className="flex justify-between items-center h-10 px-4 bg-zinc-800">
       <h1 className="text-xl font-bold">LAMA2</h1>
       <h1 className="text-md opacity-50">v{version}</h1>
     </div>
