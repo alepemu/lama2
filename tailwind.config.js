2ba9e41b10c229095801be0d3e9da7379b0dcaf5
alejandropm <geo.al3x@gmail.com>
2024-03-18 20:59:11 +0100

    Add tailwindcss-animate plugin to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -36,5 +38,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 };
